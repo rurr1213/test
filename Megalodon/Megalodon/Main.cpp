a5a802b713c78751d74a3dcd44af2197b2751d79
rurr1213@gmail.com <rurr1213@gmail.com>
2022-07-15 18:08:58 -0700

    Almost working with both projects

--- Megalodon/Megalodon/Main.cpp
+++ Megalodon/Megalodon/Main.cpp
@@ -9,13 +9,39 @@
 //
 //*********************************************************
 
-#include "stdafx.h"
+#include "..\D3D12Raytracing\D3D12RaytracingProceduralGeometry\stdafx.h"
+
 
 #include "D3D12RaytracingProceduralGeometry.h"
-
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
     D3D12RaytracingProceduralGeometry sample(1280, 720, L"D3D12 Raytracing - Procedural Geometry");
     return Win32Application::Run(&sample, hInstance, nCmdShow);
 }
+
+
+/*
+#include "..\D3D12xGPU\stdafx.h"
+#include "D3D12xGPU.h"
+
+_Use_decl_annotations_
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    // Declare this process to be high DPI aware, and prevent automatic scaling 
+    HINSTANCE hUser32 = LoadLibrary(L"user32.dll");
+    if (hUser32)
+    {
+        typedef BOOL(WINAPI* LPSetProcessDPIAware)(void);
+        LPSetProcessDPIAware pSetProcessDPIAware = (LPSetProcessDPIAware)GetProcAddress(hUser32, "SetProcessDPIAware");
+        if (pSetProcessDPIAware)
+        {
+            pSetProcessDPIAware();
+        }
+        FreeLibrary(hUser32);
+    }
+
+    D3D12xGPU sample(1280, 720, L"D3D12 xGPU sample");
+    return Win32Application::Run(&sample, hInstance, nCmdShow);
+}
+*/
